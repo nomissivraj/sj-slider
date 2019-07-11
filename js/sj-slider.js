481af88fbe5273d1b25dbf948127c36daa4f5a65
Simon Javis <SJarvis@Positive.Local>
2019-07-11 11:08:55 +0100

    working out options structure

--- js/sj-slider.js
+++ js/sj-slider.js
@@ -1,10 +1,35 @@
 var sjSlider = {};
 ((self) => {
-    self.init = () => {
-        console.log('initialised');
+    self.init = (opts) => {
+        console.log(opts);
     }
-
     self.main = () => {
         console.log('main')
     }
 })(sjSlider);
+
+let opts = {
+    sliders:[
+        {
+            sliderName: 'Category-2',
+            min:0,
+            max:10,
+            reversed: false
+        },
+        {
+            sliderName: 'Category-2',
+            textSlider: true,
+            customText: ['text1', 'text2'],
+            reversed: false
+        },
+        {
+            sliderName: 'Category-3',
+            textSlider: true,
+            customText: ['string1', 'string2'],
+            reversed: true
+        }
+    ]
+        
+};
+
+sjSlider.init(opts)
